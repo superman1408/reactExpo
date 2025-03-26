f9539afd78ba5eb26cfe1579c5d4e19424e962c3
superman1408 <superman14082017@gmail.com>
2025-03-26 16:03:48 +0530

    expo Application running without error but tailwind is not working...

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500">Uber Clone</Text>
+      <StatusBar />
+    </SafeAreaView>
+  );
+}
